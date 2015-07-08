712cef194972d16608b631ee705962426c3be045
newnowave <gdrapps@hotmail.com>
2015-07-08 08:03:34 -0700

    first commit

---
+++ likeAtron.rb
@@ -0,0 +1,34 @@
+require 'selenium-webdriver'
+require 'minitest'
+require 'minitest/autorun'
+require 'pry'
+
+class LikeAtron < Minitest::Test
+
+  def setup
+    @driver = Selenium::WebDriver.for :chrome
+    @base_url = 'https://www.facebook.com'
+    @driver.navigate.to @base_url
+  end
+
+  def test_login
+    email_input = @driver.find_element(:id, 'email')
+    password_input = @driver.find_element(:id, 'pass')
+    email_input.send_keys('')
+    password_input.send_keys('')
+    submit_button =  @driver.find_element(:id, 'u_0_x')
+    submit_button.click
+    @driver.navigate.to (@base_url + '/Daniche')
+    profile_pic = @driver.find_element(:class, 'profilePic')
+    profile_pic.click
+    like_button =  @driver.find_element(:class, 'like')
+    like_button.click
+    next_photo = @driver.find_element(:class, 'next')
+    next_photo.click
+  end
+
+  def teardown
+    @driver.quit
+  end
+
+end
